32899445b139f3dc89fd370938a220d5af04714a
cmong0516 <cmong0516@gmail.com>
2023-02-02 11:16:02 +0900

    2023.02.02

--- src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -18,6 +18,9 @@ public class AutoProxyConfig {
 
 //    @Bean
     // 스프링은 AnnotationAwareAspectJAutoProxyCreator 라는 빈 후처리기를 자동으로 등록해준다.
+    // 포인트컷은 프록시 적용 여부와 어드바이스 적용 여부를 판단해준다.
+    // 따라서 패키지와 메서드 이름을 정밀하게 지정하여야 한다.
+    // AspectJExpressionPointcut
     public Advisor advisor1(LogTrace logTrace) {
         //pointcut
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
@@ -41,6 +44,10 @@ public class AutoProxyConfig {
     public Advisor advisor3(LogTrace logTrace) {
         //pointcut
         AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+
+        // hello.proxy.app 하위의 모든 패키지
+        // *(..) 모든 메서드
+        // && !execution(* hello.proxy.app..noLog(..) noLog 는 제외한다.
         pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))");
         //advice
         LogTraceAdvice advice = new LogTraceAdvice(logTrace);
